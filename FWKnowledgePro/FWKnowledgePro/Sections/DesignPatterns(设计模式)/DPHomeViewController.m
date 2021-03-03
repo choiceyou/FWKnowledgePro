97fe83c2781077ac51194b3d97a5d74e19845344
xfg <853299701@qq.com>
2021-03-03 11:46:52 +0800

    p

--- FWKnowledgePro/FWKnowledgePro/Sections/DesignPatterns(设计模式)/DPHomeViewController.m
+++ FWKnowledgePro/FWKnowledgePro/Sections/DesignPatterns(设计模式)/DPHomeViewController.m
@@ -59,6 +59,8 @@ static NSString *const kModeName = @"modeName";
                     @"迪米特法则（Law of Demeter）又叫作最少知识原则（The Least Knowledge Principle）：一个类对于其他类知道的越少越好，就是说一个对象应当对其他对象有尽可能少的了解,只和朋友通信，不和陌生人说话",
             ]
         },
+        
+        // 大致有23种设计模式
         @{
             kTypeName : @"一、创建型",
             kModeName : @[
